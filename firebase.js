5cb156ad02d494b0ed4d02611b7ef582ce44bd06
Sakoty <black.stagekouhei@icloud.com>
2026-05-03 09:48:18 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,11 @@
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID"
+   apiKey: "AIzaSyByoa1ka43tDqJgGlPqlbRr9zvQ8QmcG_E",
+  authDomain: "tdrbgm-b1d43.firebaseapp.com",
+  projectId: "tdrbgm-b1d43",
+  storageBucket: "tdrbgm-b1d43.firebasestorage.app",
+  messagingSenderId: "214873908586",
+  appId: "1:214873908586:web:326cb800c98efd50adf113",
+  measurementId: "G-W2D68D1K8R"
 };
 
 firebase.initializeApp(firebaseConfig);
